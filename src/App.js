a34b7aac4ad9be067583d5c14fe689a165707d29
Nicolas Corbalan <outreef@hotmail.com>
2019-11-23 12:56:31 +0100

    working with API

--- src/App.js
+++ src/App.js
@@ -35,22 +35,37 @@ class App extends Component {
   }
 
   onInputChange = event => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.input});
-    app.models
-      .predict(
-        Clarifai.COLOR_MODEL,
-        'https://samples.clarifai.com/face-det.jpg'
-      )
-      .then(
-        function(response) {},
-        function(err) {
-          // there was an error
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}));
+            })
+            .catch(console.log);
         }
-      );
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
